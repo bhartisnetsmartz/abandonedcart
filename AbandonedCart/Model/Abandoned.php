f7410c2f2ac8928fa53f67655b9f9061684893a9
Puja Kumari <puja.kumari@netsmartz.net>
2023-06-21 11:41:03 +0530

    change send after field

--- AbandonedCart/Model/Abandoned.php
+++ AbandonedCart/Model/Abandoned.php
@@ -3,20 +3,25 @@ namespace Ntz\AbandonedCart\Model;
 
 class Abandoned extends \Magento\Framework\Model\AbstractModel
 {
-    // /**
-    //  * Abandoned constructor.
-    //  *
-    //  * @param \Magento\Framework\Model\Context                             $context            context
-    //  * @param \Magento\Framework\Registry                                  $registry           registry
-    //  * @param ResourceModel\Abandoned\CollectionFactory                    $abandoned          abandoned
-    //  * @param \Magento\Framework\Stdlib\DateTime                           $dateTime           dateTime
-    //  * @param array                                                        $data               data
-    //  * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource           resource
-    //  * @param \Magento\Framework\Data\Collection\AbstractDb|null           $resourceCollection resourceCollection
-    //  */
-    public function __construct(  
+    /**
+     * Abandoned constructor.
+     *
+     * @param \Magento\Framework\Model\Context                             $context            context
+     * @param \Magento\Framework\Registry                                  $registry           registry
+     * @param ResourceModel\Abandoned\CollectionFactory                    $abandoned          abandoned
+     * @param \Magento\Framework\Stdlib\DateTime                           $dateTime           dateTime
+     * @param array                                                        $data               data
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource           resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null           $resourceCollection resourceCollection
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
         public \Ntz\AbandonedCart\Model\ResourceModel\Abandoned\CollectionFactory $abandonedCollectionFactory,
         public \Magento\Framework\Stdlib\DateTime $dateTime,
+        array $data = [],
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null
     ) {
         parent::__construct(
             $context,
@@ -38,11 +43,11 @@ class Abandoned extends \Magento\Framework\Model\AbstractModel
         $this->_init(\Ntz\AbandonedCart\Model\ResourceModel\Abandoned::class);
     }
 
-    // /**
-    //  * Prepare data to be saved to database.
-    //  *
-    //  * @return $this
-    //  */
+    /**
+     * Prepare data to be saved to database.
+     *
+     * @return $this
+     */
     public function beforeSave()
     {
         parent::beforeSave();
@@ -54,13 +59,13 @@ class Abandoned extends \Magento\Framework\Model\AbstractModel
         return $this;
     }
 
-    // /**
-    //  * LoadByQuoteId
-    //  *
-    //  * @param int $quoteId quoteId
-    //  *
-    //  * @return mixed
-    //  */
+    /**
+     * LoadByQuoteId
+     *
+     * @param int $quoteId quoteId
+     *
+     * @return mixed
+     */
     public function loadByQuoteId($quoteId, $storeId)
     {
         $collection = $this->abandonedCollectionFactory->create()
